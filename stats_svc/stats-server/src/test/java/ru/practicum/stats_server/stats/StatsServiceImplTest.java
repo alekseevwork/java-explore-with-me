68496685caa120385dbd730bef3fa74282f2466d
Artem <alekseevwork7@gmail.com>
2024-11-03 12:29:59 +0200

    fix: code style

--- stats_svc/stats-server/src/test/java/ru/practicum/stats_server/stats/StatsServiceImplTest.java
+++ stats_svc/stats-server/src/test/java/ru/practicum/stats_server/stats/StatsServiceImplTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
